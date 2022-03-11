5824b21a1127108761c7284454777c53f5f7b8b6
v-max07 <vusandebnath09@gmail.com>
2022-03-11 12:12:02 +0600

    fixed repeat liked items

--- js/app.js
+++ js/app.js
@@ -43,12 +43,14 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
 
+        document.getElementById("liked").textContent = '';
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
 
+        
         displayReportedPosts();
     }
 };
@@ -159,7 +161,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
